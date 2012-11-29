5951a5bc26d35bb6b983ebeaa0524132f8023130
Abraxis <>
2012-11-29 18:22:21 +0100

    Added logging

--- src/org/abraxis/rmqtest/Publish.java
+++ src/org/abraxis/rmqtest/Publish.java
@@ -3,12 +3,18 @@ package org.abraxis.rmqtest;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
  
 public class Publish {
 
 	private final static String QUEUE_NAME = "qHello";
 
 	public static void main(String[] args) throws Exception {
+		Logger logger = LoggerFactory.getLogger(Publish.class);
+		logger.info("Hi!");
+		
+		
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
 		Connection connection = factory.newConnection();
@@ -18,7 +24,8 @@ public class Publish {
 		String message = "Hello World!";
 		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
 		System.out.println(" [x] Sent '" + message + "'");
-
+		logger.info("LOG: [x] Sent '" + message + "'");
+		
 		channel.close();
 		connection.close();
 
